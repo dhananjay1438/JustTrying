6fcb4d85c96b314432bae8392517589ad6d855a7
Dhananjay Panage <dhananjaypanage11@gmail.com>
2018-11-26 00:52:23 +0530

    THis is just a simple commit!

---
+++ SinglyLinkedLIst.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node* next;
+};
+struct node* root = NULL;
+void addAtBegin() {
+    struct node* new_node = (struct node*) malloc(sizeof(struct node));
+    printf("Enter data:");
+    scanf("%d",&new_node -> data);
+    new_node -> next = NULL;
+    if (root == NULL)
+        root = new_node;
+    else {
+        new_node -> next = root;
+        root = new_node;
+    }
+}
+int main(void) {
+
+
+    return 0;
+}
